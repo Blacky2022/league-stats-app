24b98e882735a8c823a3e34bf62cf48add42685e
Fegi176 <fegi176@gmail.com>
2023-06-19 18:52:26 +0200

    WorkingCharts

--- client/src/components/PatchSelect/PatchSelect.jsx
+++ client/src/components/PatchSelect/PatchSelect.jsx
@@ -1,12 +1,28 @@
 import React, { useState, useEffect, useCallback } from "react";
 import "./PatchSelect.css";
 import ChampionChanges from "../ChampionChanges/ChampionChanges";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+} from "recharts";
 
 export function PatchSelect() {
   const [selectedStartPatch, setSelectedStartPatch] = useState(1);
   const [selectedEndPatch, setSelectedEndPatch] = useState(23);
   const [championData, setChampionData] = useState({});
+  const [compareChampion, setCompareChampion] = useState("");
+  const [filteredChampions, setFilteredChampions] = useState([]);
+
   const championName = window.location.pathname.split("/").pop();
+
+  const handleCompareChampionChange = (event) => {
+    setCompareChampion(event.target.value);
+  };
 
   const handleStartPatchChange = (event) => {
     setSelectedStartPatch(parseInt(event.target.value));
@@ -35,15 +51,254 @@ export function PatchSelect() {
         const patchName = `Patch ${patchNumber}`;
         newData[patchName] = data;
       });
+
+      if (compareChampion) {
+        const compareData = await Promise.all(
+          Array.from({ length: end - start + 1 }, (_, index) =>
+            fetch(
+              `http://localhost:3001/performance/${compareChampion}/stats/${
+                start + index
+              }`
+            ).then((response) => response.json())
+          )
+        );
+
+        compareData.forEach((data, index) => {
+          const patchNumber = start + index;
+          const patchName = `Patch ${patchNumber} - Compare`;
+          newData[patchName] = data;
+        });
+      }
+
       setChampionData(newData);
     } catch (error) {
       console.error(error);
     }
-  }, [championName, selectedStartPatch, selectedEndPatch]);
+  }, [championName, selectedStartPatch, selectedEndPatch, compareChampion]);
 
   useEffect(() => {
+    async function fetchChampionSummary() {
+      try {
+        const response = await fetch(
+          "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-summary.json"
+        );
+        const data = await response.json();
+        setFilteredChampions(data.slice(1));
+      } catch (error) {
+        console.log("Error fetching champion summary:", error);
+      }
+    }
+    fetchChampionSummary();
     fetchChampionData();
-  }, [fetchChampionData]);
+  }, [fetchChampionData, compareChampion]);
+
+  const WinRateChart = () => {
+    const chartData = Object.entries(championData).flatMap(([patch, data]) => {
+      if (patch.includes("Compare")) return [];
+      const comparePatch = `Patch ${parseInt(patch.split(" ")[1])} - Compare`;
+      const compareData = championData[comparePatch];
+      return Object.entries(data).map(([role, stats]) => ({
+        patch,
+        role,
+        win: stats.Win,
+        compareWin:
+          compareData && compareData[role] ? compareData[role].Win : null,
+      }));
+    });
+
+    const roles = Array.from(new Set(chartData.map((data) => data.role)));
+
+    return (
+      <div>
+        {roles.map((role) => (
+          <div key={role}>
+            <h3>Role: {role}</h3>
+            <LineChart
+              width={800}
+              height={400}
+              data={chartData.filter((data) => data.role === role)}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="patch" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="win"
+                stroke="#8884d8"
+                name="Normal Champ Win Rate"
+              />
+              {compareChampion && (
+                <Line
+                  type="monotone"
+                  dataKey="compareWin"
+                  stroke="#82ca9d"
+                  name="Compare Champ Win Rate"
+                />
+              )}
+            </LineChart>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const PickBanRateChart = () => {
+    const chartData = Object.entries(championData).flatMap(([patch, data]) => {
+      if (patch.includes("Compare")) return [];
+      const comparePatch = `Patch ${parseInt(patch.split(" ")[1])} - Compare`;
+      const compareData = championData[comparePatch];
+      return Object.entries(data).map(([role, stats]) => ({
+        patch,
+        role,
+        pick: stats.Pick,
+        ban: stats.Ban,
+        comparePick:
+          compareData && compareData[role] ? compareData[role].Pick : null,
+        compareBan:
+          compareData && compareData[role] ? compareData[role].Ban : null,
+      }));
+    });
+
+    const roles = Array.from(new Set(chartData.map((data) => data.role)));
+
+    return (
+      <div>
+        {roles.map((role) => (
+          <div key={role}>
+            <h3>Role: {role}</h3>
+            <LineChart
+              width={800}
+              height={400}
+              data={chartData.filter((data) => data.role === role)}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="patch" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="pick"
+                stroke="#82ca9d"
+                name="Normal Champ Pick Rate"
+              />
+              <Line
+                type="monotone"
+                dataKey="ban"
+                stroke="#ffc658"
+                name="Normal Champ Ban Rate"
+              />
+              {compareChampion && (
+                <Line
+                  type="monotone"
+                  dataKey="comparePick"
+                  stroke="#82ca9d"
+                  name="Compare Champ Pick Rate"
+                />
+              )}
+              {compareChampion && (
+                <Line
+                  type="monotone"
+                  dataKey="compareBan"
+                  stroke="#ffc658"
+                  name="Compare Champ Ban Rate"
+                />
+              )}
+            </LineChart>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const SelectedChampionChart = () => {
+    return (
+      <div>
+        <h2>Win Rate</h2>
+        <WinRateChart />
+        <h2>Pick and Ban Rates</h2>
+        <PickBanRateChart />
+      </div>
+    );
+  };
+
+  const ComparedChampionTable = (
+    <table className="champions-table">
+      <thead>
+        <tr>
+          <th>Patch</th>
+          <th>Role</th>
+          <th>Tier</th>
+          <th>Score</th>
+          <th>Trend</th>
+          <th>Win</th>
+          <th>Pick</th>
+          <th>Ban</th>
+          <th>KDA</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(championData).map(([patch, data]) => (
+          <React.Fragment key={patch}>
+            {patch.includes("Compare") &&
+              Object.entries(data).map(([role, stats]) => (
+                <tr key={`${patch}-${role}`}>
+                  <td>{patch}</td>
+                  <td>{role}</td>
+                  <td>{stats.Tier}</td>
+                  <td>{stats.Score}</td>
+                  <td>{stats.Trend}</td>
+                  <td>{stats.Win}</td>
+                  <td>{stats.Pick}</td>
+                  <td>{stats.Ban}</td>
+                  <td>{stats.KDA}</td>
+                </tr>
+              ))}
+          </React.Fragment>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  const selectedChampionTable = (
+    <table className="champions-table">
+      <thead>
+        <tr>
+          <th>Patch</th>
+          <th>Role</th>
+          <th>Tier</th>
+          <th>Score</th>
+          <th>Trend</th>
+          <th>Win</th>
+          <th>Pick</th>
+          <th>Ban</th>
+          <th>KDA</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(championData).map(([patch, data]) => (
+          <React.Fragment key={patch}>
+            {!patch.includes("Compare") &&
+              Object.entries(data).map(([role, stats]) => (
+                <tr key={`${patch}-${role}`}>
+                  <td>{patch}</td>
+                  <td>{role}</td>
+                  <td>{stats.Tier}</td>
+                  <td>{stats.Score}</td>
+                  <td>{stats.Trend}</td>
+                  <td>{stats.Win}</td>
+                  <td>{stats.Pick}</td>
+                  <td>{stats.Ban}</td>
+                  <td>{stats.KDA}</td>
+                </tr>
+              ))}
+          </React.Fragment>
+        ))}
+      </tbody>
+    </table>
+  );
 
   return (
     <div>
@@ -72,57 +327,29 @@ export function PatchSelect() {
       <div>Selected Start Patch: {selectedStartPatch}</div>
 
       <div>Selected End Patch: {selectedEndPatch}</div>
+      <div>
+        Compare with:
+        <select value={compareChampion} onChange={handleCompareChampionChange}>
+          {filteredChampions.map((champion) => (
+            <option key={champion.name} value={champion.name}>
+              {champion.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <SelectedChampionChart />
 
-      {Object.keys(championData).length > 0 && (
-        <div>
-          <table className="champions-table">
-            <thead>
-              <tr>
-                <th>Patch</th>
-                <th>Role</th>
-                <th>Tier</th>
-                <th>Score</th>
-                <th>Trend</th>
-                <th>Win</th>
-                <th>Pick</th>
-                <th>Ban</th>
-                <th>KDA</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(championData).map(([patch, data]) =>
-                Object.entries(data).map(([role, stats]) => (
-                  <tr key={`${patch}-${role}`}>
-                    <td>{patch}</td>
-                    <td>{role}</td>
-                    <td>{stats.Tier}</td>
-                    <td>{stats.Score}</td>
-                    <td>{stats.Trend}</td>
-                    <td>{stats.Win}</td>
-                    <td>{stats.Pick}</td>
-                    <td>{stats.Ban}</td>
-                    <td>{stats.KDA}</td>
-                  </tr>
-                ))
-              )}
-            </tbody>
-          </table>
-          {selectedStartPatch !== selectedEndPatch && (
-            <ChampionChanges
-              selectedStartPatch={selectedStartPatch}
-              selectedEndPatch={selectedEndPatch}
-            />
-          )}
-          {Object.keys(championData).map((patch) => (
+      {selectedChampionTable}
+      {ComparedChampionTable}
+    </div>
+  );
+}
+
+/* {Object.keys(championData).map((patch) => (
             <img
               key={patch}
               className="patch-image"
               src={`/aktualizacje/patch_${patch.split(" ")[1]}.jpg`}
               alt={patch}
             />
-          ))}
-        </div>
-      )}
-    </div>
-  );
-}
+          ))} */
--- client/src/pages/ChampionsPerformance/ChampionsPerformance.jsx
+++ client/src/pages/ChampionsPerformance/ChampionsPerformance.jsx
@@ -1,58 +1,70 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import './ChampionsPerformance.css'
-import { Navbar } from '../../components/Navbar/Navbar'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./ChampionsPerformance.css";
+import { Navbar } from "../../components/Navbar/Navbar";
 export function ChampionsPerformance() {
-	const [championSummary, setChampionSummary] = useState(null)
-	const [searchQuery, setSearchQuery] = useState('')
-	const [filteredChampions, setFilteredChampions] = useState([])
+  const [championSummary, setChampionSummary] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredChampions, setFilteredChampions] = useState([]);
 
-	useEffect(() => {
-		async function fetchChampionSummary() {
-			try {
-				const response = await fetch(
-					'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-summary.json'
-				)
-				const data = await response.json()
-				setChampionSummary(data)
-				setFilteredChampions(data.slice(1))
-			} catch (error) {
-				console.log('Error fetching champion summary:', error)
-			}
-		}
+  useEffect(() => {
+    async function fetchChampionSummary() {
+      try {
+        const response = await fetch(
+          "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-summary.json"
+        );
+        const data = await response.json();
+        setChampionSummary(data);
+        setFilteredChampions(data.slice(1));
+      } catch (error) {
+        console.log("Error fetching champion summary:", error);
+      }
+    }
 
-		fetchChampionSummary()
-	}, [])
+    fetchChampionSummary();
+  }, []);
 
-	const handleSearch = event => {
-		const query = event.target.value
-		setSearchQuery(query)
-		const filtered = championSummary
-			? championSummary.filter(champion => champion.name.toLowerCase().includes(query.toLowerCase()))
-			: []
-		setFilteredChampions(filtered)
-	}
+  const handleSearch = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+    const filtered = championSummary
+      ? championSummary.filter((champion) =>
+          champion.name.toLowerCase().includes(query.toLowerCase())
+        )
+      : [];
+    setFilteredChampions(filtered);
+  };
 
-	if (!championSummary) {
-		return <div>Loading champion summary...</div>
-	}
+  if (!championSummary) {
+    return <div>Loading champion summary...</div>;
+  }
 
-	return (
-		<>
-			<Navbar />
-			<div className='container'>
-				<input className='search-input' type='text' value={searchQuery} onChange={handleSearch} placeholder='Search champion...' />
-				{filteredChampions.map(champion => (
-					<Link to={`/performance/${champion.name}`} className='champion' key={champion.id}>
-						<img
-							className='avatar'
-							src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${champion.id}.png`}
-							alt={champion.name}
-						/>
-						<span className='name'>{champion.name}</span>
-					</Link>
-				))}
-			</div>
-		</>
-	)
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <input
+          className="search-input"
+          type="text"
+          value={searchQuery}
+          onChange={handleSearch}
+          placeholder="Search champion..."
+        />
+        {filteredChampions.map((champion) => (
+          <Link
+            to={`/performance/${champion.name}`}
+            className="champion"
+            key={champion.id}
+          >
+            <img
+              className="avatar"
+              src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/${champion.id}.png`}
+              alt={champion.name}
+            />
+            <span className="name">{champion.name}</span>
+          </Link>
+        ))}
+      </div>
+    </>
+  );
 }
