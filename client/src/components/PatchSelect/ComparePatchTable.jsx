cab87d9a77941a7dc773fdf0bd26dfb1545783d3
Fegi176 <fegi176@gmail.com>
2023-06-21 23:02:40 +0200

    final i guess

--- client/src/components/PatchSelect/ComparePatchTable.jsx
+++ client/src/components/PatchSelect/ComparePatchTable.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
 
-export default function ComparePatchTable({ patchData, comparedChampion, championName }) {
+export default function ComparePatchTable({
+  patchData,
+  comparedChampion,
+  championName,
+}) {
   const { patchNotes, comparedPatchNotes } = patchData;
 
   if (!patchNotes || patchNotes.length === 0) {
     return (
-      <div>
-        <p>{championName} was not changed during this time period or data is missing.</p>
-        <table>
+      <div className="compare-container">
+        <p>
+          {championName} was not changed during this time period or data is
+          missing.
+        </p>
+        <table className="champions-table">
           <thead>
             <tr>
               <th>Patch Number</th>
@@ -29,9 +36,12 @@ export default function ComparePatchTable({ patchData, comparedChampion, champio
 
   if (!comparedPatchNotes || comparedPatchNotes.length === 0) {
     return (
-      <div>
-        <p>{comparedChampion} was not changed during this time period or data is missing.</p>
-        <table>
+      <div className="compare-container">
+        <p>
+          {comparedChampion} was not changed during this time period or data is
+          missing.
+        </p>
+        <table className="champions-table">
           <thead>
             <tr>
               <th>Patch Number</th>
@@ -53,48 +63,50 @@ export default function ComparePatchTable({ patchData, comparedChampion, champio
 
   return (
     <>
-      <div>
-        <h2>{championName}</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Patch Number</th>
-              <th>Update Description</th>
-              <th>Buff</th>
-            </tr>
-          </thead>
-          <tbody>
-            {patchNotes.map((note, index) => (
-              <tr key={index}>
-                <td>{note.patchNumber}</td>
-                <td>{note.opis}</td>
-                <td>{note.buff ? 'Yes' : 'No'}</td>
+      <div className="compare-container">
+        <div>
+          <h2>{championName}</h2>
+          <table className="champions-table">
+            <thead>
+              <tr>
+                <th>Patch Number</th>
+                <th>Update Description</th>
+                <th>Buff</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {patchNotes.map((note, index) => (
+                <tr key={index}>
+                  <td>{note.patchNumber}</td>
+                  <td>{note.opis}</td>
+                  <td>{note.czy_buff ? "Yes" : "No"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
 
-      <div>
-        <h2>{comparedChampion}</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Patch Number</th>
-              <th>Update Description</th>
-              <th>Buff</th>
-            </tr>
-          </thead>
-          <tbody>
-            {comparedPatchNotes.map((note, index) => (
-              <tr key={index}>
-                <td>{note.patchNumber}</td>
-                <td>{note.opis}</td>
-                <td>{note.buff ? 'Yes' : 'No'}</td>
+        <div>
+          <h2>{comparedChampion}</h2>
+          <table className="champions-table">
+            <thead>
+              <tr>
+                <th>Patch Number</th>
+                <th>Update Description</th>
+                <th>Buff</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {comparedPatchNotes.map((note, index) => (
+                <tr key={index}>
+                  <td>{note.patchNumber}</td>
+                  <td>{note.opis}</td>
+                  <td>{note.czy_buff ? "Yes" : "No"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
