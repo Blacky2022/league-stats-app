3f4908c9cbf6bed59034c5f53c2260c07e4d3c32
Bartlomiej Mlynarski <bartek.mlynarski18@gmail.com>
2023-06-22 20:44:09 +0200

    init

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import userModel from '../models/user.model'
 
 export const signup = async (req: Request, res: Response): Promise<void> => {
