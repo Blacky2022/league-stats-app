b3181d136a2b75f2d4b5d68d3ff79bf01ba4b952
Bartlomiej Mlynarski <bartek.mlynarski18@gmail.com>
2023-06-22 17:11:23 +0200

    data endpoint

--- server/controllers/worlds.controller.ts
+++ server/controllers/worlds.controller.ts
@@ -10,3 +10,19 @@ export const getChampions = async (req: Request, res: Response): Promise<void> =
 		res.status(500).json({ error: 'Error retrieving champions from the database.' })
 	}
 }
+
+export const getCompare = async (req: Request, res: Response): Promise<void> => {
+	const { name, comparedName } = req.params
+	try {
+		const firstChampion = await WorldsChampionModel.findOne({ champion: name })
+		const compareChampion = await WorldsChampionModel.findOne({ champion: comparedName })
+		if (!firstChampion || !compareChampion) {
+			res.status(404).json({ error: 'One or both champions not found.' })
+			return
+		}
+		res.json({ firstChampion, compareChampion })
+	} catch (error) {
+		console.error('Error retrieving champions:', error)
+		res.status(500).json({ error: 'Error retrieving champions from the database.' })
+	}
+}
--- server/routes/worlds.ts
+++ server/routes/worlds.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
-import { getChampions } from '../controllers/worlds.controller'
+import { getCompare, getChampions } from '../controllers/worlds.controller'
 
 export const worldsRouter = Router()
 
 worldsRouter.get('/champions', getChampions)
+            .get('/compare/:name/with/:comparedName', getCompare)
